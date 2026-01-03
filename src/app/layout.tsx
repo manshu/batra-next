b18982f4e04134455c919dc21ec1f79621c6fce9
Himanshu Batra <himanshu@batra.io>
2026-01-03 15:16:27 -0500

    WiP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+
+        {/* Google Analytics */}
+        <GoogleAnalytics gaId="G-Z418CLJ6XH" />
       </body>
     </html>
   );
